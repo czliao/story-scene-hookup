5dcf1a123bc5ad34fd712a11faeb5bc93853f744
czliao <joe@liao.cz>
2015-05-22 13:52:43 -0700

    added timeout before trigger. added presentation mode

--- the-hook.js
+++ the-hook.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
 	$('.swiper-slide').bind('DOMSubtreeModified', function(e) { // Track segment changes
+		setTimeout(triggerHook, 50);
+	});
+
+	function triggerHook () {
 		var hook = $('.swiper-slide-active .slide-hook'); // Find custom hook in segment content, see notes at end.
 		if (hook) {
 			var slide_num = $(hook).attr('data-slide');
 			$('iframe').contents().find('*[nr="'+ slide_num +'"] .slideButton').click(); // Trigger the jump by faking a click on the hidden slide button.
 		}
-	})
+	}
+
+	// EXTRA: Presentation Mode //
+
+	var pMode = false;
+
+	window.addEventListener('keydown', function(k) {
+		switch(k.keyCode) {
+        case 'p':
+        	if (!pMode) {
+        		$('.linkSocialContainer').hide();
+        		$('iframe').contents().find('#slideDeckContainer').hide();
+        		pMode = true;
+        	} else {
+        		$('.linkSocialContainer').show();
+        		$('iframe').contents().find('#slideDeckContainer').show();
+        		pMode = false;
+        	}
+            break;
+        default:
+            break;
+    }
 });
+
+
+
 
 // NOTES //
 
