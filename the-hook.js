c2e0b403de093c014460a85970ddc33e832b857a
czliao <joe@liao.cz>
2015-05-22 13:55:54 -0700

    Fixed brackets error

--- the-hook.js
+++ the-hook.js
@@ -1,36 +1,37 @@
 $(document).ready(function() {
-	$('.swiper-slide').bind('DOMSubtreeModified', function(e) { // Track segment changes
-		setTimeout(triggerHook, 50);
-	});
+    $('.swiper-slide').bind('DOMSubtreeModified', function(e) { // Track segment changes
+        setTimeout(triggerHook, 50);
+    });
 
-	function triggerHook () {
-		var hook = $('.swiper-slide-active .slide-hook'); // Find custom hook in segment content, see notes at end.
-		if (hook) {
-			var slide_num = $(hook).attr('data-slide');
-			$('iframe').contents().find('*[nr="'+ slide_num +'"] .slideButton').click(); // Trigger the jump by faking a click on the hidden slide button.
-		}
-	}
+    function triggerHook () {
+        var hook = $('.swiper-slide-active .slide-hook'); // Find custom hook in segment content, see notes at end.
+        if (hook) {
+            var slide_num = $(hook).attr('data-slide');
+            $('iframe').contents().find('*[nr="'+ slide_num +'"] .slideButton').click(); // Trigger the jump by faking a click on the hidden slide button.
+        }
+    }
 
-	// EXTRA: Presentation Mode //
+    // EXTRA: Presentation Mode //
 
-	var pMode = false;
+    var pMode = false;
 
-	window.addEventListener('keydown', function(k) {
-		switch(k.keyCode) {
+    window.addEventListener('keydown', function(k) {
+        switch(k.keyCode) {
         case 'p':
-        	if (!pMode) {
-        		$('.linkSocialContainer').hide();
-        		$('iframe').contents().find('#slideDeckContainer').hide();
-        		pMode = true;
-        	} else {
-        		$('.linkSocialContainer').show();
-        		$('iframe').contents().find('#slideDeckContainer').show();
-        		pMode = false;
-        	}
+            if (!pMode) {
+                $('.linkSocialContainer').hide();
+                $('iframe').contents().find('#slideDeckContainer').hide();
+                pMode = true;
+            } else {
+                $('.linkSocialContainer').show();
+                $('iframe').contents().find('#slideDeckContainer').show();
+                pMode = false;
+            }
             break;
         default:
             break;
-    }
+        }
+    });
 });
 
 
@@ -39,4 +40,4 @@ $(document).ready(function() {
 // NOTES //
 
 // Add this to each segment in the story map to hook to a particular bookmark in the scene.
-//<span class="slide-hook" data-slide="2"></span>
+// <span class="slide-hook" data-slide="2"></span>
