17d4cf34daf862e691ead45bdb39b95146a1d911
czliao <joe@liao.cz>
2015-05-21 18:47:53 -0700

    Test flight
    Everything hooked up.

--- the-hook.js
+++ the-hook.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-	$('.sections').scroll(function(e) {
-		console.log('scrolled');
+	$('.swiper-slide').bind('DOMSubtreeModified', function(e) { // Track segment changes
+		var hook = $('.swiper-slide-active .slide-hook'); // Find custom hook in segment content, see notes at end.
+		if (hook) {
+			var slide_num = $(hook).attr('data-slide');
+			$('iframe').contents().find('*[nr="'+ slide_num +'"] .slideButton').click(); // Trigger the jump by faking a click on the hidden slide button.
+		}
 	})
-	// alert("I'm done.");
-	$('.swiper-slide').click(function(e) {
-		console.log(e);
-	});
 });
+
+// NOTES //
+
+// Add this to each segment in the story map to hook to a particular bookmark in the scene.
+//<span class="slide-hook" data-slide="2"></span>
