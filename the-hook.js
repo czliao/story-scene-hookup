93730f196497262a4cf176d1beea10e84fa8b69f
czliao <joe@liao.cz>
2015-05-21 17:51:16 -0700

    Second commit to see if it updates the story map
    fingers crossed.

--- the-hook.js
+++ the-hook.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-	// $('.sections').scroll(function(e) {
-	// 	console.log('scrolled');
-	// })
-	alert("I'm done.");
+	$('.sections').scroll(function(e) {
+		console.log('scrolled');
+	})
+	// alert("I'm done.");
+	$('.swiper-slide').click(function(e) {
+		console.log(e);
+	});
 });
